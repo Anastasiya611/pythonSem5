a64a8ff43883f8412dc3c57877a22979734c723f
Anastasiya <asa611@mail.ru>
2023-03-02 18:50:37 +0300

    hw5

---
+++ z26.py
@@ -0,0 +1,15 @@
+# Задача 26: Напишите программу, которая на вход принимает два числа A и B, 
+# и возводит число А в целую степень B с помощью рекурсии.
+# A = 3; B = 5 -> 243 (3⁵)
+# A = 2; B = 3 -> 8
+
+
+def result(a,b):
+    if b == 1:
+        return a
+    else:
+        return (a * result(a, b-1))
+        
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+print (result(a,b))
